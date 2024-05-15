f46d05f4cc2af064eab603f433e8e673beb196e2
iivanov <selivanov@realx.tech>
2024-05-15 15:25:05 +0300

    dfssdsfsdfd

--- src/app/signal.ts
+++ src/app/signal.ts
@@ -1,6 +1,11 @@
-import OneSignal from 'react-onesignal';
+import OneSignal from 'react-onesignal'
 
 export default async function runOneSignal() {
-  await OneSignal.init({ appId: 'b40b7cc7-13dc-4662-8b48-efa668f9b72a', allowLocalhostAsSecureOrigin: true});
-  OneSignal.Slidedown.promptPush();
+	await OneSignal.init({
+		appId: 'c4f58ac6-03b8-4711-9f98-559eb7e625f0',
+		safari_web_id: 'web.onesignal.auto.35f9fdf2-e602-4832-95b7-1c199bdb2bd7',
+		allowLocalhostAsSecureOrigin: true,
+	})
+	OneSignal.Slidedown.promptPush()
 }
+s
