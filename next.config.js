1c63f94d2ec7e938c5504bd539226cba4c51fe9b
iivanov <selivanov@realx.tech>
2024-05-15 10:52:22 +0300

    change to public

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
-/** @type {import('next').NextConfig} */
-
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
 });
 
-const nextConfig={
-  ...withPWA({
-    dest:'public',
-    register:true ,
-    skipWaiting:true,
-  })
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
 
