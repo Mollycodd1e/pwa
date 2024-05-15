123cb12c8b8cebe1384a7b272feaec5628bb77af
iivanov <selivanov@realx.tech>
2024-05-15 15:05:02 +0300

    sdfsdfsdsdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -135,7 +135,7 @@ export default function Home() {
 				OneSignalDeferred.push(function(OneSignal){' '}
 				{
 					//@ts-ignore
-					typeof window !== 'undefined' && window.OneSignal.init({
+					typeof window !== 'undefined' && OneSignal.init({
 						appId: 'c4f58ac6-03b8-4711-9f98-559eb7e625f0',
 						safari_web_id: 'web.onesignal.auto.35f9fdf2-e602-4832-95b7-1c199bdb2bd7',
 						notifyButton: {
